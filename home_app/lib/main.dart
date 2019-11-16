ea65cb2544166aef4d984bccc22ff8613c853676
Kaloyan Dimitrov <47056188+Kaloyan-Dimitrov@users.noreply.github.com>
2019-11-16 15:50:17 +0200

    sending lisence to rpi

--- home_app/lib/main.dart
+++ home_app/lib/main.dart
@@ -7,12 +7,8 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(),
+    return Center(
+      child: MyHomePage(),
     );
   }
 }
