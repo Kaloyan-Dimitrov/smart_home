5eb769d0a49cf02be18f31a56603c7c447289c07
Kaloyan Dimitrov <47056188+Kaloyan-Dimitrov@users.noreply.github.com>
2019-11-16 18:21:30 +0200

    Added a simple led script to check gpio

--- RPi/main.js
+++ RPi/main.js
@@ -1,5 +1,6 @@
 const admin = require('firebase-admin');
-
+const Gpio = require('onoff').Gpio;
+var LEDPin = new Gpio(4, 'out');
 let serviceAccount = require('./SmartLamps-f44d9ee76876.json');
 
 admin.initializeApp({
@@ -10,5 +11,6 @@ let db = admin.firestore();
 db.collection("lampStates").doc("main")
     .onSnapshot(function (doc) {
         console.log("Current data: ", doc.data());
+        LEDPin.writeSync(doc.data().isOn ? 1 : 0);
         // Here it should send a signal to the wifi module and the wifi module should turn the relay respectivaly on or off
     });
