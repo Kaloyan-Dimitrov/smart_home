c8dd80302a9b248be1f200221d0761f179a4c910
Kaloyan Dimitrov <47056188+Kaloyan-Dimitrov@users.noreply.github.com>
2019-11-16 15:52:45 +0200

    chenged broken reference

--- RPi/main.js
+++ RPi/main.js
@@ -1,6 +1,6 @@
 const admin = require('firebase-admin');
 
-let serviceAccount = require('c:/Users/kaloy/Desktop/Projects/smart_home/RPi/smartlamps-c8015-839601f81fdc.json');
+let serviceAccount = require('./smartlamps-c8015-839601f81fdc.json');
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
